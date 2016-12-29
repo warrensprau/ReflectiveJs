98bc0c7f9209b2425c8646c8e057983f05ca0b76
Warren <warren@deepcovelabs.com>
2016-12-29 15:17:38 -0800

    Switch to cloud

--- ReflectiveJs.Server.Api/Global.asax.cs
+++ ReflectiveJs.Server.Api/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ReflectiveJs.Server.Api
@@ -14,8 +13,6 @@ namespace ReflectiveJs.Server.Api
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
